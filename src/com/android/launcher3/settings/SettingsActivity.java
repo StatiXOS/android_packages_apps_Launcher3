d48966e58fedc4cbd0c877497851f08b583558f2
DennySPB <dennyspb@gmail.com>
2019-09-27 15:36:58 +0300

    Launcher3: Add restart action
    Change-Id: I562ef0baead993a0b1e2881a399ea97a98513f3f
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- src/com/android/launcher3/settings/SettingsActivity.java
+++ src/com/android/launcher3/settings/SettingsActivity.java
@@ -135,7 +135,9 @@ public class SettingsActivity extends FragmentActivity
     }
 
     @Override
-    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) { }
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+                Utilities.restart(this);
+   }
 
     private boolean startPreference(String fragment, Bundle args, String key) {
         if (Utilities.ATLEAST_P && getSupportFragmentManager().isStateSaved()) {
