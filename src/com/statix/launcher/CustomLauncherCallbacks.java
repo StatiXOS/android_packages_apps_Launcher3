39b76c864065ff0a16c9be37f48fc0504723af57
Chris Crump <chris@cypheros.co>
2019-10-12 21:37:51 -0400

    CustomLauncher: Switch to advanced Google Feed
    A newer feed library that supports more API's such as a proper dark ui
    
    Change-Id: I8bc381aae5daa622f1a44749fb1bceed0cde81ee

--- src/com/statix/launcher/CustomLauncherCallbacks.java
+++ src/com/statix/launcher/CustomLauncherCallbacks.java
@@ -24,6 +24,8 @@ import com.android.launcher3.Utilities;
 import com.android.launcher3.model.data.AppInfo;
 import com.android.launcher3.settings.SettingsActivity;
 
+import com.google.android.libraries.gsa.launcherclient.ClientOptions;
+import com.google.android.libraries.gsa.launcherclient.ClientService;
 import com.google.android.libraries.gsa.launcherclient.LauncherClient;
 
 import java.io.FileDescriptor;
@@ -38,6 +40,7 @@ public class CustomLauncherCallbacks implements LauncherCallbacks,
 
     private OverlayCallbackImpl mOverlayCallbacks;
     private LauncherClient mLauncherClient;
+    private SharedPreferences mPrefs;
 
     private boolean mStarted;
     private boolean mResumed;
@@ -49,11 +52,11 @@ public class CustomLauncherCallbacks implements LauncherCallbacks,
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        SharedPreferences prefs = Utilities.getPrefs(mLauncher);
+        mPrefs = Utilities.getPrefs(mLauncher);
         mOverlayCallbacks = new OverlayCallbackImpl(mLauncher);
-        mLauncherClient = new LauncherClient(mLauncher, mOverlayCallbacks, getClientOptions(prefs));
+        mLauncherClient = new LauncherClient(mLauncher, mOverlayCallbacks, getClientOptions(mPrefs));
         mOverlayCallbacks.setClient(mLauncherClient);
-        prefs.registerOnSharedPreferenceChangeListener(this);
+        mPrefs.registerOnSharedPreferenceChangeListener(this);
     }
 
     public void onResume() {
@@ -85,7 +88,30 @@ public class CustomLauncherCallbacks implements LauncherCallbacks,
     }
 
     public void onDestroy() {
-        mLauncherClient.onDestroy();
+        if (!mLauncherClient.isDestroyed()) {
+            mLauncherClient.getActivity().unregisterReceiver(mLauncherClient.mInstallListener);
+        }
+
+        mLauncherClient.setDestroyed(true);
+        mLauncherClient.getBaseService().disconnect();
+        if (mLauncherClient.getOverlayCallback() != null) {
+            mLauncherClient.getOverlayCallback().mClient = null;
+            mLauncherClient.getOverlayCallback().mWindowManager = null;
+            mLauncherClient.getOverlayCallback().mWindow = null;
+            mLauncherClient.setOverlayCallback(null);
+        }
+
+        ClientService service = mLauncherClient.getClientService();
+        LauncherClient client = service.getClient();
+        if (client != null && client.equals(mLauncherClient)) {
+            service.mWeakReference = null;
+            if (!mLauncherClient.getActivity().isChangingConfigurations()) {
+                service.disconnect();
+                if (ClientService.sInstance == service) {
+                    ClientService.sInstance = null;
+                }
+            }
+        }
 
         Utilities.getPrefs(mLauncher).unregisterOnSharedPreferenceChangeListener(this);
     }
@@ -107,7 +133,10 @@ public class CustomLauncherCallbacks implements LauncherCallbacks,
     }
 
     public void onDetachedFromWindow() {
-        mLauncherClient.onDetachedFromWindow();
+        if (!mLauncherClient.isDestroyed()) {
+            mLauncherClient.getEventInfo().parse(0, "detachedFromWindow", 0.0f);
+            mLauncherClient.setParams(null);
+        }
     }
 
     @Override
@@ -138,18 +167,23 @@ public class CustomLauncherCallbacks implements LauncherCallbacks,
     }
 
     @Override
-    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+    public void onSharedPreferenceChanged(SharedPreferences prefs, String key) {
         if (SettingsActivity.MINUS_ONE_KEY.equals(key)) {
-            mLauncherClient.setClientOptions(getClientOptions(sharedPreferences));
+            ClientOptions clientOptions = getClientOptions(prefs);
+            if (clientOptions.options != mLauncherClient.mFlags) {
+                mLauncherClient.mFlags = clientOptions.options;
+                if (mLauncherClient.getParams() != null) {
+                    mLauncherClient.updateConfiguration();
+                }
+                mLauncherClient.getEventInfo().parse("setClientOptions ", mLauncherClient.mFlags);
+            }
         }
     }
 
-    private LauncherClient.ClientOptions getClientOptions(SharedPreferences prefs) {
+    private ClientOptions getClientOptions(SharedPreferences prefs) {
         boolean hasPackage = CustomUtils.hasPackageInstalled(mLauncher, SEARCH_PACKAGE);
         boolean isEnabled = prefs.getBoolean(SettingsActivity.MINUS_ONE_KEY, true);
-        return new LauncherClient.ClientOptions(hasPackage && isEnabled,
-                true, /* enableHotword */
-                true /* enablePrewarming */
-        );
+        int canUse = hasPackage && isEnabled ? 1 : 0;
+        return new ClientOptions(canUse | 2 | 4 | 8);
     }
 }
