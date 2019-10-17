8f3ee6ea72e3ab56bb1b7a2077a30e11a2a95232
paphonb <paphonb@gmail.com>
2019-10-17 18:35:05 +0700

    Add simple iconpack support
    based on
    https://gerrit.dirtyunicorns.com/c/4780/
    
    paphonb's edits:
    - Override IconProvider instead of modifying the cache mechanism
    - Listen to pref changes in ConfigMonitor
    
    Change-Id: I69188ae2a6cddd163b211b29e139fb84cd79e80d

---
+++ src/com/android/launcher3/CustomIconProvider.java
@@ -0,0 +1,28 @@
+package com.android.launcher3;
+
+import android.content.Context;
+import android.content.pm.LauncherActivityInfo;
+import android.graphics.drawable.Drawable;
+
+import androidx.annotation.Keep;
+
+@Keep
+public class CustomIconProvider extends IconProvider {
+
+    private Context mContext;
+
+    public CustomIconProvider(Context context) {
+        mContext = context;
+    }
+
+    @Override
+    public Drawable getIcon(LauncherActivityInfo info, int iconDpi, boolean flattenDrawable) {
+        Drawable icon = super.getIcon(info, iconDpi, flattenDrawable);
+        IconPack iconPack = IconPackProvider.loadAndGetIconPack(mContext);
+        if (iconPack != null) {
+            return iconPack.getIcon(info, null, info.getLabel());
+        } else {
+            return icon;
+        }
+    }
+}
