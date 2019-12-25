7c41dd52b832a005f9df3aea99fb5626967ba3eb
Chris Crump <chris@cypheros.co>
2019-12-25 02:00:14 -0500

    CustomLauncher: Add support for Google QuickSearchBox
    Moves QSB to the hotseat and adds support for basic Google search functionality. This iteration does not support extended features (ex. Google Doodle) but has plans to support these features later on
    
    Change-Id: Ia905b475e133c82814745e0cd037d07f7b41d1a9

--- src/com/statix/launcher/search/AppItemInfoWithIcon.java
+++ src/com/statix/launcher/search/AppItemInfoWithIcon.java
@@ -13,18 +13,23 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.statix.launcher;
+package com.statix.launcher.search;
 
-import com.android.launcher3.Launcher;
-import com.android.launcher3.LauncherCallbacks;
+import android.content.Intent;
 
-public class CustomLauncher extends Launcher {
+import com.android.launcher3.ItemInfoWithIcon;
+import com.android.launcher3.util.ComponentKey;
 
-    public CustomLauncher() {
-        setLauncherCallbacks(new CustomLauncherCallbacks(this));
+public class AppItemInfoWithIcon extends ItemInfoWithIcon {
+
+    public Intent mIntent;
+
+    public AppItemInfoWithIcon(ComponentKey componentKey) {
+        mIntent = new Intent("android.intent.action.MAIN").addCategory("android.intent.category.LAUNCHER").setComponent(componentKey.componentName).addFlags(270532608);
+        user = componentKey.user;
     }
 
-    public LauncherCallbacks getLauncherCallbacks() {
-        return mLauncherCallbacks;
+    public Intent getIntent() {
+        return mIntent;
     }
 }
