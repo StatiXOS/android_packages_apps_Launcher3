0c23d527d6538353b66e72f1e949c6683ae54fad
Anay Wadhera <awadhera@berkeley.edu>
2020-02-02 16:59:14 -0800

    Launcher3: systemize icon pack [2/2]
    * use systemsettinglistpref in support vendor
    
    Change-Id: Ie1d109704dea54ef1379eb20de14a85b3e02f110

--- src/com/android/launcher3/settings/IconPackPreference.java
+++ src/com/android/launcher3/settings/IconPackPreference.java
@@ -18,8 +18,6 @@ import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.TextView;
 
-import androidx.preference.ListPreference;
-
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
@@ -29,8 +27,9 @@ import java.util.Map;
 import com.android.launcher3.IconPackProvider;
 import com.android.launcher3.R;
 
+import com.statix.support.preferences.SystemSettingListPreference;
 
-public class IconPackPreference extends ListPreference {
+public class IconPackPreference extends SystemSettingListPreference {
 
     private final PackageManager pm;
 
@@ -51,7 +50,10 @@ public class IconPackPreference extends ListPreference {
 
     private void init() {
         String currentPack = IconPackProvider.getCurrentIconPack(getContext());
-        if (currentPack.isEmpty()) {
+        if (currentPack == null) {
+            currentPack = "";
+        }
+        if (currentPack == "") {
             setNone();
         } else {
             try {
