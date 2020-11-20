905a7f0e8ac690c294ffa963f4e8d753c2251440
Dil3mm4 <dil3mm4.dev@gmail.com>
2020-11-20 21:05:29 +0000

    Recents: add kill app button
    While at it, rework the layout to be suitable for more than 2 buttons
    
    Change-Id: I691f17dd5aa6af92ac4d8b70643a93966afee52e

--- quickstep/recents_ui_overrides/src/com/android/quickstep/views/RecentsView.java
+++ quickstep/recents_ui_overrides/src/com/android/quickstep/views/RecentsView.java
@@ -56,6 +56,7 @@ import android.animation.ObjectAnimator;
 import android.animation.ValueAnimator;
 import android.annotation.TargetApi;
 import android.app.ActivityManager;
+import android.app.IActivityManager;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
@@ -86,6 +87,7 @@ import android.view.ViewGroup;
 import android.view.accessibility.AccessibilityEvent;
 import android.view.accessibility.AccessibilityNodeInfo;
 import android.view.animation.Interpolator;
+import android.widget.Button;
 import android.widget.ListView;
 
 import androidx.annotation.Nullable;
@@ -231,6 +233,7 @@ public abstract class RecentsView<T extends StatefulActivity> extends PagedView 
     private final RecentsModel mModel;
     private final int mTaskTopMargin;
     private final ClearAllButton mClearAllButton;
+    private Button mKillAppButton;
     private final Rect mClearAllButtonDeadZoneRect = new Rect();
     private final Rect mTaskViewDeadZoneRect = new Rect();
 
@@ -404,6 +407,7 @@ public abstract class RecentsView<T extends StatefulActivity> extends PagedView 
         mClearAllButton = (ClearAllButton) LayoutInflater.from(context)
                 .inflate(R.layout.overview_clear_all_button, this, false);
         mClearAllButton.setOnClickListener(this::dismissAllTasks);
+
         mTaskViewPool = new ViewPool<>(context, this, R.layout.task, 20 /* max size */,
                 10 /* initial size */);
 
@@ -504,6 +508,8 @@ public abstract class RecentsView<T extends StatefulActivity> extends PagedView 
     public void init(OverviewActionsView actionsView) {
         mActionsView = actionsView;
         mActionsView.updateHiddenFlags(HIDDEN_NO_TASKS, getTaskViewCount() == 0);
+        mKillAppButton = (Button) mActionsView.findViewById(R.id.kill_app);
+        mKillAppButton.setOnClickListener(this::killTask);
     }
 
     @Override
@@ -1552,6 +1558,22 @@ public abstract class RecentsView<T extends StatefulActivity> extends PagedView 
     private void dismissAllTasks(View view) {
         runDismissAnimation(createAllTasksDismissAnimation(DISMISS_TASK_DURATION));
         mActivity.getUserEventDispatcher().logActionOnControl(TAP, CLEAR_ALL_BUTTON);
+    }
+
+    @SuppressWarnings("unused")
+    private void killTask(View view) {
+        IActivityManager am = ActivityManager.getService();
+        TaskView tv = getNextPageTaskView();
+        Task task = tv.getTask();
+        String pkgname = task.key.getPackageName();
+        if (task != null) {
+            try {
+                am.killBackgroundProcesses(pkgname, UserHandle.USER_CURRENT);
+            } catch (Throwable t) {
+                //TODO: handle exception
+            }
+            dismissTask(tv, true, true);
+        }
     }
 
     private void dismissCurrentTask() {
