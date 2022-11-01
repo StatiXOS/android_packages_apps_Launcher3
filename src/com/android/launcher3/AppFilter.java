c0101b19deceb0149c1ebfb5418a00c18011e965
Anay Wadhera <anay1018@gmail.com>
2022-11-02 00:23:19 +0900

    Launcher3: Make AppFilter a ResourceBasedOverride
    Change-Id: I9165d5e06976946544eedd2adddb1ffd7349d343

--- src/com/android/launcher3/AppFilter.java
+++ src/com/android/launcher3/AppFilter.java
@@ -3,6 +3,8 @@ package com.android.launcher3;
 import android.content.ComponentName;
 import android.content.Context;
 
+import com.android.launcher3.util.ResourceBasedOverride;
+
 import java.util.Arrays;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -10,15 +12,25 @@ import java.util.stream.Collectors;
 /**
  * Utility class to filter out components from various lists
  */
-public class AppFilter {
+public class AppFilter implements ResourceBasedOverride {
 
     private final Set<ComponentName> mFilteredComponents;
 
+    // Should not be used anymore.
     public AppFilter(Context context) {
         mFilteredComponents = Arrays.stream(
                 context.getResources().getStringArray(R.array.filtered_components))
                 .map(ComponentName::unflattenFromString)
                 .collect(Collectors.toSet());
+    }
+
+    /**
+     * Retrieve instance of this object that can be overridden in runtime based on the build
+     * variant of the application.
+     */
+    public static AppFilter newInstance(Context context) {
+        return Overrides.getObject(AppFilter.class,
+                context.getApplicationContext(), R.string.app_filter_class);
     }
 
     public boolean shouldShowApp(ComponentName app) {
