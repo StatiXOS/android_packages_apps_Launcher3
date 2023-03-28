e03011b94d13e02d0b9ca2d577393b033462c04f
Pranav Vashi <neobuddy89@gmail.com>
2023-03-28 20:56:22 +0530

    Launcher3: Fix blurry recents issue with live tile
    * Issue description: https://github.com/LawnchairLauncher/lawnchair/issues/2762
    
    Change-Id: I9e86d9b8ccc73401808a1d25181eaf0719c57739
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- quickstep/src/com/android/quickstep/views/RecentsView.java
+++ quickstep/src/com/android/quickstep/views/RecentsView.java
@@ -3097,6 +3097,7 @@ public abstract class RecentsView<
         setEnableFreeScroll(true);
         setEnableDrawingLiveTile(mCurrentGestureEndTarget == GestureState.GestureEndTarget.RECENTS);
         Log.d(TAG, "onGestureAnimationEnd - mEnableDrawingLiveTile: " + mEnableDrawingLiveTile);
+        setRunningTaskViewShowScreenshot(true);
         setRunningTaskHidden(false);
         startIconFadeInOnGestureComplete();
         setTaskIconVisible(true);
