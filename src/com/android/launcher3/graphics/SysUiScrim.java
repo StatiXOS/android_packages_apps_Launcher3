7cab0578d6b31b592a0c5cb06fa59a03b7ec943b
Sourajit Karmakar <sourajitk@statixos.com>
2023-07-10 23:16:00 -0400

    Launcher3: Return true for HideSysUiScrim
    It's awful.
    
    Change-Id: I1c412d7ba9a2c865df5b059735e53fbe1b500a7b

--- src/com/android/launcher3/graphics/SysUiScrim.java
+++ src/com/android/launcher3/graphics/SysUiScrim.java
@@ -101,7 +101,7 @@ public class SysUiScrim implements View.OnAttachStateChangeListener {
 
         mTopMaskHeight = ResourceUtils.pxFromDp(TOP_MASK_HEIGHT_DP, dm);
         mBottomMaskHeight = ResourceUtils.pxFromDp(BOTTOM_MASK_HEIGHT_DP, dm);
-        mHideSysUiScrim = Themes.getAttrBoolean(view.getContext(), R.attr.isWorkspaceDarkText);
+        mHideSysUiScrim = true;
 
         mTopMaskBitmap = mHideSysUiScrim ? null : createDitheredAlphaMask(mTopMaskHeight,
                 new int[]{0x3DFFFFFF, 0x0AFFFFFF, 0x00FFFFFF},
